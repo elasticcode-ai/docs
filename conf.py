36dd7dd985f086d3767b745694ed3a3d8e0cd473
Darren Govoni <darren@rapidquest.ai>
2022-10-22 20:49:25 -0400

    Updates

--- conf.py
+++ conf.py
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = 'images/flow.svg'
+html_logo = 'images/elasticcode.svg'
 #html_favicon = 'pyfi16.ico'
 html_favicon = 'images/favicon2.ico'
 
