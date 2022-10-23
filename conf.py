7a0d032ac548fd95e5b71532aa56c6499166a984
Darren Govoni <darren@rapidquest.ai>
2022-10-23 07:53:55 -0400

    Updates

--- conf.py
+++ conf.py
@@ -56,7 +56,6 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = 'images/elasticcode.svg'
-#html_favicon = 'pyfi16.ico'
 html_favicon = 'images/favicon2.ico'
 
 html_theme_options = {
